daba93b7d1c609ce9facd780add689163f50383a
RogerAllan <rogerallan4004@gmail.com>
2023-07-03 12:42:12 -0300

    main

---
+++ coffee.py
@@ -0,0 +1,75 @@
+from dash import Dash, dcc, html
+import pandas as pd
+import plotly.express as px
+
+class DataCleaner:
+    def __init__(self, filename):
+        self.filename = filename
+        self.df = pd.DataFrame()
+
+    def load_data(self):
+        try:
+            self.df = pd.read_csv(self.filename, sep=',')
+            self.df.dropna(inplace=True)  # Drop rows with missing values
+        except pd.errors.ParserError as e:
+            print(f"Erro ao ler o arquivo CSV: {e}")
+            # Faça o tratamento de erro adequado.
+
+    def validate_data(self):
+        # Verificar colunas inválidas
+        required_columns = [
+            "ID", "Country of Origin", "Farm Name", "Lot Number", "Mill", "ICO Number",
+            "Company", "Altitude", "Region", "Producer", "Number of Bags", "Bag Weight",
+            "In-Country Partner", "Harvest Year", "Grading Date", "Owner", "Variety",
+            "Status", "Processing Method", "Aroma", "Flavor", "Aftertaste", "Acidity",
+            "Body", "Balance", "Uniformity", "Clean Cup", "Sweetness", "Overall",
+            "Defects", "Total Cup Points", "Moisture Percentage", "Category One Defects",
+            "Quakers", "Color", "Category Two Defects", "Expiration", "Certification Body",
+            "Certification Address", "Certification Contact"
+        ]
+        missing_columns = [col for col in required_columns if col not in self.df.columns]
+        if missing_columns:
+            print(f"Colunas ausentes no arquivo CSV: {missing_columns}")
+            # Faça o tratamento de erro adequado.
+
+        # Verificar valores ausentes
+        if self.df.isnull().values.any():
+            print("Existem valores ausentes no arquivo CSV.")
+            # Faça o tratamento de erro adequado.
+
+    def clean_data(self):
+        # Realize a limpeza de dados específica, se necessário
+        pass
+
+    def convert_date_column(self, column, formats):
+        for fmt in formats:
+            try:
+                self.df[column] = pd.to_datetime(self.df[column], format=fmt)
+                break
+            except ValueError:
+                continue
+
+app = Dash(__name__)
+
+# Load the data and perform data cleaning/validation
+filename = 'df_arabica_clean.csv'
+cleaner = DataCleaner(filename)
+cleaner.load_data()
+cleaner.validate_data()
+cleaner.clean_data()
+df_cleaned = cleaner.df
+
+# Create the initial figure for the bar graph
+fig_cf = px.bar(df_cleaned,x='Farm Name', y='Region')
+fig = px.scatter(df_cleaned,x='Overall', y='Flavor', color= 'Farm Name')
+# Define the Dash layout
+app.layout = html.Div(
+    children=[
+        dcc.Graph(figure=fig_cf),  # Display the bar graph
+        dcc.Graph(figure=fig)
+    ]
+)
+
+# Run the Dash application
+if __name__ == '__main__':
+    app.run_server(debug=True)
